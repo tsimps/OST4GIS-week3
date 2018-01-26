100f1c35f6e9840abd343f350e7d6db4c1233a0c
Andrew <tsimps@sas.upenn.edu>
2018-01-26 15:11:00 -0500

    Completed Lab 1 and Lab 2 Part 1
    Halfway through update

--- lab/lab1/part1.js
+++ lab/lab1/part1.js
@@ -7,7 +7,9 @@ Instructions: "Write a function that adds one to the number provided"
 Example: "plusOne(2) should return 3"
 ===================== */
 
-var plusOne = function() {};
+var plusOne = function(num) {
+  return num + 1;
+};
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -17,7 +19,7 @@ Instructions: "Write a function, age, that takes a birth year and returns an age
 Example: "age(2000) should return 18"
 ===================== */
 
-var age = function(birth) {};
+var age = function(birth) {return 2017 - birth; };
 
 console.log('age success:', age(1971) === 46);
 
@@ -26,7 +28,7 @@ Instructions: "Write a function that returns true for numbers over 9000 and fals
 Example: "over9000(22) should return false"
 ===================== */
 
-var over9000 = function() {};
+var over9000 = function(num) {return num > 9000; };
 
 console.log('over9000 success:', over9000(9001) === true && over9000(12) === false);
 
@@ -35,7 +37,9 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-var valueAtKey = function() {};
+var valueAtKey = function(obj, key) {
+  return obj[key];
+};
 
 console.log('valueAtKey success:', valueAtKey({'foo': 'bar'}, 'foo') === 'bar');
 
@@ -45,7 +49,9 @@ Example: "y(0, 0, 0) should return 0; y(1, 1, 1) should return 2"
 Remember: The standard mathematical expression for such a function is y=mx+b
 ===================== */
 
-var y = function() {};
+var y = function(m, x, b) {
+  return m * x + b;
+};
 
 console.log('y success:', y(12, 1, 12) === 24);
 
@@ -53,8 +59,13 @@ console.log('y success:', y(12, 1, 12) === 24);
 Instructions: "Write a function which counts the number of times a value occurs in an array "
 Example: "countItem(['a', 'b', 'a'], 'a') should return 2"
 ===================== */
+var counter = 0;
 
-var countItem = function() {};
+var countItem = function(arr, val) {
+  for (var i in arr) {
+    if (arr[i] === val) { counter = counter + 1; }
+  }
+  return counter;
+};
 
 console.log('countItem success:', countItem([1, 2, 3, 4, 5, 4, 4], 4) === 3);
-
--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -7,7 +7,7 @@ Functions can be passed as values to other functions. Each exercise here builds 
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {};
+var justOne = function() {return 1;};
 
 console.log('justOne success:', justOne() === 1);
 
@@ -15,7 +15,7 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {};
+var isEven = function(num) { return num % 2 === 0; };
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -24,7 +24,7 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {};
+var justFalse = function() {return isEven(justOne()); };
 
 console.log('justFalse success:', justFalse() === false);
 
@@ -32,7 +32,7 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {};
+var not = function(boo) { return !boo; };
 
 console.log('not success:', not(true) === false);
 
@@ -41,7 +41,7 @@ Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {};
+var isOdd = function(num) {return not(isEven(num)); };
 
 console.log('isOdd success:', isOdd(4) === false);
 
@@ -49,7 +49,17 @@ console.log('isOdd success:', isOdd(4) === false);
 Instructions: Write a function that takes a list of numbers and returns a list with only numbers above 10
 ===================== */
 
-var filterOutLessThan10 = function() {};
+var filterOutLessThan10 = function(list) {
+  var newList = [];
+
+  for (var n in list) {
+    if (list[n] > 10) {
+      newList.push(list[n]);
+    }
+  }
+  console.log(newList);
+  return newList;
+};
 
 console.log('filterOutLessThan10 success:', filterOutLessThan10([4, 11]) === [11]);
 
@@ -60,7 +70,8 @@ Instructions: Let's bring it all together. Write a function that filters a list 
               2. a function that takes a value and returns true (to keep a number) or false (to toss it out)
 ===================== */
 
-var filter = function(array, func) {};
+var filter = function(array, func) {
+
+};
 
 console.log('filter success:', filter([4, 11], isOdd) === [11]);
-
